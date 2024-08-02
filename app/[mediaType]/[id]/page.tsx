25e9a47094a684f0595e8a3200d1dad2696d0d75
chirayu-xx <cboss1436@gmail.com>
2024-08-02 21:42:20 +0530

    trailerIssueFixed

--- app/[mediaType]/[id]/page.tsx
+++ app/[mediaType]/[id]/page.tsx
@@ -11,7 +11,7 @@ import useFetch from "@/hooks/useFetch";
 import { fetchImageUrl } from "@/src/utils/urlFetch";
 import { getApiConfiguration } from "@/src/redux/features/homeSlice";
 import { useDispatch } from "react-redux";
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 import SeasonList from "@/src/components/SeasonList/SeasonList";
 
 type Props = {};
@@ -19,25 +19,22 @@ type Props = {};
 const DetailPage = (props: Props) => {
   const dispatch = useDispatch();
   const { mediaType, id } = useParams();
-  const { data:mediaData , loading:dataLoading } = useFetch(`/${mediaType}/${id}`);
+  const { data: mediaData, loading: dataLoading } = useFetch(
+    `/${mediaType}/${id}`
+  );
   const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
   const { data: credits, loading: creditsLoading } = useFetch(
     `/${mediaType}/${id}/credits`
   );
-  var index = data?.results.findIndex((element: Element) =>
-    element.name.includes("Official Trailer")
-  );
-  const types = ["Trailer", "Teaser", "Clip"];
+  const terms = ["Official Trailer", "Trailer", "Teaser", "Clip"];
 
-      index = types.reduce((acc, type) => {
-        return acc === -1
-          ? trailerData?.results.findIndex(
-              (element: Element) => element.type === type
-            )
-          : acc;
-      }, index);  
-  
-
+  var index =
+    data?.results.findIndex((element: Element) =>
+      terms.some((term) => element.name.includes(term))
+    ) ?? 0;
+    if(index === -1){
+      index = 0;
+    }
   useEffect(() => {
     const fetchImageData = async () => {
       const url = await fetchImageUrl();
@@ -47,26 +44,28 @@ const DetailPage = (props: Props) => {
   }, []);
   return (
     <motion.div
-    initial={{opacity:0 , y:20}}
-    animate={{opacity:1, y:0}}
-    exit={{opacity:0 , y:20}}
-    transition={{
-      type: "spring",
-      stiffness: 260,
-      damping: 15,
-      duration: 1,
-      delay: 1
-    }}
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 20 }}
+      transition={{
+        type: "spring",
+        stiffness: 260,
+        damping: 15,
+        duration: 1,
+        delay: 1,
+      }}
     >
-      <DetailsBanner data={mediaData} loading={dataLoading} video={data?.results?.[index]} crew={credits?.crew} />
+      <DetailsBanner
+        data={mediaData}
+        loading={dataLoading}
+        video={data?.results?.[index]}
+        crew={credits?.crew}
+      />
       <Cast data={credits?.cast} loading={creditsLoading} />
-      {
-        mediaType === "tv" && <SeasonList id={id} data={mediaData?.seasons} loading={dataLoading} />
-      }
-      {
-        data?.results.length && 
-        <VideosSection data={data} loading={loading} />
-      }
+      {mediaType === "tv" && (
+        <SeasonList id={id} data={mediaData?.seasons} loading={dataLoading} />
+      )}
+      {data?.results.length && <VideosSection data={data} loading={loading} />}
       <Similar mediaType={mediaType} id={id} />
       <Recommendation mediaType={mediaType} id={id} />
     </motion.div>
